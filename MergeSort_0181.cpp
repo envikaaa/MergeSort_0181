2f8a773230dc0ba5ecb9ff43b1ef34d254c24c0b
envikaaa <envikapriyatna31@gmail.com>
2026-04-03 15:11:33 +0700

    void output

--- MergeSort_0181.cpp
+++ MergeSort_0181.cpp
@@ -83,4 +83,19 @@ void mergeSort(int low, int high)
     }
 
     // step 5
+    // step 5
+    for (int x = low; x <= high; x++)
+    {
+        arr[x] = B[x];
+    }
+}
+
+void output()
+{
+    cout << "\nData setelah diurutkan (Merge Sort): ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
